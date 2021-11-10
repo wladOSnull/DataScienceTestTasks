74ef59ce6f840cb413a76fc523dedb86c5be1b23
wlad1 <wlad1@DESKTOP-CPN3U6O>
2021-11-10 19:32:01 +0200

    Add project files.

---
+++ Task1Fibonacci/ClassForFibonacci.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataScienceTestTasks
+{
+    class ClassForFibonacci
+    {
+        // simple "wrapper" for visual demonstration of Fibonacci() function
+        public static void Main()
+        {
+            Console.Write("Task 1 :: Fibonacci\n" + "Input number of Fibonacci value: ");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("result: {0}" , Fibonacci(0, 1, 1, number));
+
+            Console.ReadKey();
+        }
+
+        // the developed function for Task 1
+        private static double Fibonacci(double first, double second, int iterator, int numberOfValue)
+        {
+            if (iterator <= numberOfValue)
+                return Fibonacci(second, first + second, iterator + 1, numberOfValue);
+            else
+                return first;
+        }
+    }
+}
---
+++ Task2LinearEquations/ClassForLinearEquations.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataScienceTestTasks
+{
+    class ClassForSolvingSLE
+    {
+        // simple "wrapper" for visual demonstration of ThreeLinearEquations() function
+        public static void Main()
+        {
+            Console.WriteLine("Task 2 :: three linear equations (hardcoded)");
+
+            int[] result = ThreeLinearEquations();
+            
+            Console.Write("result: ");
+            for (int i = 0; i < result.Length; i++)
+                Console.Write("{0} ", result[i]);
+
+            Console.ReadKey();
+        }
+
+        // the developed function for Task 2
+        public static int[] ThreeLinearEquations()
+        {
+            int[,] array1 = { { 1, 2, 3},
+                            { 0, 1, 2},
+                            { 2, 0, 0} };
+
+            int[] array2 = { 1, 1, 0 };
+
+            return new int[] { array1[0, 0] * array2[0] + array1[0, 1] * array2[1] + array1[0, 2] * array2[2],
+                                array1[1, 0] * array2[0] + array1[1, 1] * array2[1] + array1[1, 2] * array2[2],
+                                array1[2, 0] * array2[0] + array1[2, 1] * array2[1] + array1[2, 2] * array2[2]};
+        }
+    }
+}
